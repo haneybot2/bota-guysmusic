b3d06485b5428a6a0e9ea3dc6116cd73fa24f4d2
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-09-23 22:16:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
-const Util = require('discord.js');
+const { Client, Util } = require('discord.js');
 const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 const YouTube = require('simple-youtube-api');
-const youtube = new YouTube('AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8');
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
 const ytdl = require('ytdl-core');
 const gif = require('gif-search');
@@ -259,7 +259,16 @@ let dur = `${hrs}${min}${sec}`
 		id: video.id,
 		title: Util.escapeMarkdown(video.title),
 		duration: dur,
-		url: `https://www.youtube.com/watch?v=${video.id}`
+		url: `https://www.youtube.com/watch?v=${video.id}`,
+		eyad:`${video.thumbnails.high.url}`,
+		best:`${video.channel.title}`,
+		bees:`${video.raw.snippet.publishedAt}`,
+		shahd:`${video.raw.kind}`,
+		zg:`${video.raw.snippet.channelId}`,
+		views:`${video.raw.views}`,
+		like:`${video.raw.likeCount}`,
+		dislike:`${video.raw.dislikeCount}`,
+		hi:`${video.raw.id}`
 	};
 	if (!serverQueue) {
 		const queueConstruct = {
@@ -325,19 +334,19 @@ function play(guild, song) {
 
 function embedFormat(queue) {
 
-	if (!queue || !queue.songs) {
-		return 'No music playing\n\u23F9 '+bar(-1)+' '+volumeIcon(100);
-	} else if (!queue.playing) {
-		return 'No music playing\n\u23F9 '+bar(-1)+' '+volumeIcon(queue.volume);
-	} else {
+  if(!queue || !queue.songs) {
+    return "No music playing\n\u23F9 "+bar(-1)+" "+volumeIcon(100);
+  } else if(!queue.playing) {
+    return "No music playing\n\u23F9 "+bar(-1)+" "+volumeIcon(queue.volume);
+  } else {
 
-		let progress = (queue.connection.dispatcher.time / queue.songs[0].msDur);
-		let prog = bar(progress);
-		let volIcon = volumeIcon(queue.volume);
-		let playIcon = (queue.connection.dispatcher.paused ? '\u23F8' : '\u25B6')
-		let dura = queue.songs[0].duration;
+    let progress = (queue.connection.dispatcher.time / queue.songs[0].msDur);
+    let prog = bar(progress);
+    let volIcon = volumeIcon(queue.volume);
+    let playIcon = (queue.connection.dispatcher.paused ? "\u23F8" : "\u25B6")
+    let dura = queue.songs[0].duration;
 
-		return playIcon + ' ' + prog + ' `[' + formatTime(queue.connection.dispatcher.time) + '/' + dura + ']`' + volIcon;
+    return playIcon + ' ' + prog + ' `[' + formatTime(queue.connection.dispatcher.time) + '/' + dura + ']`' + volIcon;
 
 
   }
@@ -345,49 +354,50 @@ function embedFormat(queue) {
 }
 
 function formatTime(duration) {
-	var milliseconds = parseInt((duration % 1000) / 100),
-	seconds = parseInt((duration / 1000) % 60),
-	minutes = parseInt((duration / (1000 * 60)) % 60),
-	hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+var milliseconds = parseInt((duration % 1000) / 100),
+seconds = parseInt((duration / 1000) % 60),
+minutes = parseInt((duration / (1000 * 60)) % 60),
+hours = parseInt((duration / (1000 * 60 * 60)) % 24);
 
-	hours = (hours < 10) ? '0' + hours : hours;
-	minutes = (minutes < 10) ? '0' + minutes : minutes;
-	seconds = (seconds < 10) ? '0' + seconds : seconds;
+hours = (hours < 10) ? "0" + hours : hours;
+minutes = (minutes < 10) ? "0" + minutes : minutes;
+seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-	return (hours > 0 ? hours + ':' : '') + minutes + ':' + seconds;
+return (hours > 0 ? hours + ":" : "") + minutes + ":" + seconds;
 }
 
 function bar(precent) {
 
-	var str = '';
+  var str = '';
 
-	for (var i = 0; i < 12; i++) {
+  for (var i = 0; i < 12; i++) {
 
-		let pre = precent
-		let res = pre * 12;
+    let pre = precent
+    let res = pre * 12;
 
-		res = parseInt(res)
+    res = parseInt(res)
 
-		if (i == res){
-			str+='\uD83D\uDD18';
-		} else {
-			str+='▬';
-		}
-	}
+    if(i == res){
+      str+="\uD83D\uDD18";
+    }
+    else {
+      str+="▬";
+    }
+  }
 
-	return str;
+  return str;
 
 }
 
 function volumeIcon(volume) {
 
-	if (volume == 0)
-		return '\uD83D\uDD07';
-	if (volume < 30)
-		return '\uD83D\uDD08';
-	if (volume < 70)
-		return '\uD83D\uDD09';
-		return '\uD83D\uDD0A';
+  if(volume == 0)
+     return "\uD83D\uDD07";
+ if(volume < 30)
+     return "\uD83D\uDD08";
+ if(volume < 70)
+     return "\uD83D\uDD09";
+ return "\uD83D\uDD0A";
 
 }
 
